1a15f7e408dbc3b2ff556761e6786d070caf5598
Luciano Brito <lucianobrito.dev@gmail.com>
2021-08-06 13:37:59 -0300

    Created middleware of error

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require('express')
 const app = express()
+
 const router = require('./src/routes/routes')
+const messageError = require('./src/messages/message-error')
 
 const url = 'http://127.0.0.1'
 const port = 3000
+const path = __dirname
 
-
-/** 
+/**
  * Middlewares */
- app.use(express.json())
- app.use('/', router)
-
-app.get('/', (req, resp, next) => {
-    resp.send('Desafio Cert Sapienta')
+app.use(express.json())
+app.use('/', router)
+app.use((error, req, res, next) => {
+    if (error) {
+        res.send(JSON.stringify(messageError(error)))
+    }
+    next()
 })
 
-app.use((error, req, res, next) => {
-    res.sendStatus(500);
+app.get('/', (req, res) => {
+    res.sendFile(path + '/src/views/index.html')
 })
 
 app.listen(port, () => {
-    console.log(`Server is running at url ${url}:${port}...`)
+    console.log('Server is running...\n' + `url: \'${url}:${port}\'`)
 })
